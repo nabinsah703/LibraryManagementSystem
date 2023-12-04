ff53f609f4c04d42ae850632d79cf3282de91fc1
nabin sah <nabinsah703@gmail.com>
2023-12-04 21:31:35 +0545

    add publisher tab and crud operation for publisher page and show data in grid view

---
+++ LMS_LibraryTraining/Admin/AddPublisher.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Title="Publisher Details" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="AddPublisher.aspx.cs" Inherits="LMS_LibraryTraining.Admin.AddPublisher" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4 border border-dark mt-3 mb-3">
+                <div class="row">
+                    <div class="col-12">
+                        <h4>Add Publisher</h4>
+                        <div class="form-group">
+                            <asp:TextBox ID="txtpublisherID" CssClass="form-control" placeHolder="Pulisher ID" runat="server"></asp:TextBox>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <asp:TextBox ID="txtpublisherName" CssClass="form-control" placeHolder="Publisher Name" runat="server"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <asp:Button ID="btnAdd" Text="Add" CssClass="btn btn-success" Width="120" Height="40" runat="server" OnClick="btnAdd_Click" />
+                    <asp:Button ID="btnUpdate" Text="Update" runat="server" Width="120" Height="40" CssClass="btn btn-success" OnClick="btnUpdate_Click" Visible="false" />
+                    <asp:Button ID="btnCancel" runat="server" Text="Cancel" CssClass="btn btn-danger" Width="120" Height="40" OnClick="btnCancel_Click" Visible="false"/>
+                </div>
+            </div>
+            <div class="col-8 border border-success mt-3 mb-3">
+                <div class="table table-responsive">
+                    <h4>Show all Publisher</h4>
+                    <asp:Repeater ID="rptRepeater" runat="server" OnItemCommand="rptRepeater_ItemCommand">
+
+                        <HeaderTemplate>
+                            <table class="table table-bordered table-hover">
+                                <thead class="alert-info">
+                                    <tr>
+                                        <th><span>Publisher ID</span></th>
+                                        <th><span>Publisher Name</span></th>
+                                        <th>&nbsp;</th>
+
+                                    </tr>
+
+                                </thead>
+                                <tbody>
+                        </HeaderTemplate>
+                        <ItemTemplate>
+                            <tr>
+                                <td><%#Eval("publisher_id") %></td>
+                                <td><%#Eval("publisher_name") %></td>
+                                <td style="width: 20%">
+                                    <asp:LinkButton ID="lnkEdit" class="table-link text-primary" CommandArgument='<%#Eval("publisher_id")%>' CommandName="edit" ToolTip="Record Edit" runat="server">
+                 <span class="fa-stack">
+                     <i class="fa fa-square fa-stack-2x"></i>
+                     <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
+
+                 </span>
+                                    </asp:LinkButton>
+                                    <asp:LinkButton ID="lnkdelete" class="table-link text-danger" CommandArgument='<%#Eval("publisher_id")%>' CommandName="delete" ToolTip="Record Delete" Text="Delete" runat="server" OnClientClick="return confirm('Do you want to delete record')">
+                  <span class="fa-stack">
+                      <i class="fa fa-square fa-stack-2x"></i>
+                      <i class="fa fa-trash fa-stack-1x fa-inverse"></i>
+
+                  </span>
+                                    </asp:LinkButton>
+                                </td>
+                            </tr>
+                        </ItemTemplate>
+                        <FooterTemplate>
+                            </tbody>
+ </table>
+                        </FooterTemplate>
+                    </asp:Repeater>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ LMS_LibraryTraining/Admin/AddPublisher.aspx.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LMS_LibraryTraining.Admin
+{
+    public partial class AddPublisher : System.Web.UI.Page
+    {
+        DBConnect conn = new DBConnect();
+        SqlCommand cmd = new SqlCommand();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                AutoGenerate();
+                BindPublisherRecord();
+
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("sp_InsertPublisher", conn.GetConnection());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@id", txtpublisherID.Text.Trim());
+            cmd.Parameters.AddWithValue("@name", txtpublisherName.Text.Trim());
+            conn.OpenConn();
+
+            if (cmd.ExecuteNonQuery() == 1)
+            {
+                conn.CloseConn();
+                Response.Write("<script>alert('Saved succcessfully')</script>");
+                clearControl();
+                BindPublisherRecord();
+                AutoGenerate();
+
+            }
+            else
+            {
+                conn.CloseConn();
+                Response.Write("<script>alert('Record not inserted')</script>");
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("sp_UpdatePublisher", conn.GetConnection());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@id", txtpublisherID.Text.Trim());
+            cmd.Parameters.AddWithValue("@name", txtpublisherName.Text.Trim());
+            conn.OpenConn();
+
+            if (cmd.ExecuteNonQuery() == 1)
+            {
+                conn.CloseConn();
+                Response.Write("<script>alert('Updated succcessfully')</script>");
+                clearControl();
+                BindPublisherRecord();
+                AutoGenerate();
+                btnAdd.Visible = true;
+                btnUpdate.Visible = false;
+                btnCancel.Visible = false;
+            }
+            else
+            {
+                conn.CloseConn();
+                Response.Write("<script>alert('Record not Updated')</script>");
+            }
+
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminHome.aspx");
+
+        }
+
+        public void AutoGenerate()
+        {
+            try
+            {
+                int r;
+                conn.OpenConn();
+                cmd = new SqlCommand("select max(publisher_id) as ID from publisher_master_tbl", conn.GetConnection());
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.Read())
+                {
+                    string d = rdr[0].ToString();
+                    if (d == "")
+                    {
+                        txtpublisherID.Text = "1001";
+                    }
+                    else
+                    {
+                        r = Convert.ToInt32(rdr[0].ToString());
+                        r = r + 1;
+                        txtpublisherID.Text = r.ToString();
+                    }
+                    txtpublisherID.ReadOnly = true;
+                }
+                conn.CloseConn();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert(" + ex.Message + ")</script>");
+            }
+        }
+        private void clearControl()
+        {
+            txtpublisherID.Text = txtpublisherName.Text = string.Empty;
+            txtpublisherName.Focus();
+        }
+
+        private void searchDataForUpdate(int? idd)
+        {
+            cmd = new SqlCommand("sp_getPublisherByID", conn.GetConnection());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@ID", idd);
+            conn.OpenConn();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
+            da.Fill(ds, "dt");
+            conn.CloseConn();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                txtpublisherID.Text = ds.Tables[0].Rows[0]["publisher_id"].ToString();
+                txtpublisherName.Text = ds.Tables[0].Rows[0]["publisher_name"].ToString();
+                btnAdd.Visible = false;
+                btnUpdate.Visible = true;
+                btnCancel.Visible = true;
+            }
+            else
+            {
+                Response.Write("<script>alert('No Record Found ... Try Again ...')</script>");
+
+            }
+        }
+
+        private void BindPublisherRecord()
+        {
+            cmd = new SqlCommand("sp_getPublisherList", conn.GetConnection());
+            cmd.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            conn.OpenConn();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            rptRepeater.DataSource = dt;
+            rptRepeater.DataBind();
+        }
+
+        protected void rptRepeater_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+
+            if (e.CommandName == "edit")
+            {
+                string[] commandArgs = e.CommandArgument.ToString().Split(new char[] { '&' });
+                string id = commandArgs[0];
+                searchDataForUpdate(Convert.ToInt32(id));
+            }
+            else if (e.CommandName == "delete")
+            {
+                string[] commandArgs = e.CommandArgument.ToString().Split(new char[] { '&' });
+                string id = commandArgs[0];
+                cmd = new SqlCommand("sp_DeletePublisher", conn.GetConnection());
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@ID", id);
+                conn.OpenConn();
+
+                if (cmd.ExecuteNonQuery() == 1)
+                {
+                    conn.CloseConn();
+                    Response.Write("<script>alert('Deleted succcessfully')</script>");
+                    clearControl();
+                    BindPublisherRecord();
+                    AutoGenerate();
+                    btnAdd.Visible = true;
+                    btnUpdate.Visible = false;
+                    btnCancel.Visible = false;
+                }
+                else
+                {
+                    conn.CloseConn();
+                    Response.Write("<script>alert('Record not Deleted')</script>");
+                }
+            }
+        }
+    }
+}
